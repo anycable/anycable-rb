c839cafcf71e8c2f7c82ee47490fb1a6b59f3d7e
Vladimir Dementyev <dementiev.vm@gmail.com>
2025-01-02 13:00:41 -0800

    tests: use health checking to wait for the server to start

--- spec/support/with_grpc_server.rb
+++ spec/support/with_grpc_server.rb
@@ -5,7 +5,20 @@ RSpec.shared_context "anycable:grpc:server" do
     @server = AnyCable.server_builder.call(AnyCable.config)
 
     @server.start
-    sleep 0.1
+
+    health_service = if GRPC_KIT
+      sock = TCPSocket.new(*AnyCable.config.rpc_host.split(":"))
+      ::Grpc::Health::V1::Health::Stub.new(sock)
+    else
+      ::Grpc::Health::V1::Health::Stub.new(AnyCable.config.rpc_host, :this_channel_is_insecure)
+    end
+
+    time = 2.0
+    loop do
+      break if health_service.check(Grpc::Health::V1::HealthCheckRequest.new({service: "anycable.RPC"})).status == :SERVING
+      raise "Server is not ready" if (time -= 0.1) < 0
+      sleep 0.1
+    end
   end
 
   after(:all) { @server.stop }
